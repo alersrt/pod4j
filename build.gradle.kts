a358ea079ced0c2e75fa12eb4975adfec072d2d0
Aleksandr <strtmg@gmail.com>
2024-11-18 22:57:54 +0700

    0.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,8 @@ repositories {
 }
 
 dependencies {
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3.sample:unixdomainsockets:3.14.9")
     implementation(project(":openapi"))
     testImplementation(platform("org.junit:junit-bom:5.10.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
--- src/main/java/io/github/alersrt/pod4j/Main.java
+++ src/main/java/io/github/alersrt/pod4j/Main.java
@@ -1,11 +1,27 @@
 package io.github.alersrt.pod4j;
 
 
+import io.github.alersrt.pod4j.openapi.ApiClient;
+import io.github.alersrt.pod4j.openapi.ApiException;
+import io.github.alersrt.pod4j.openapi.Configuration;
+import io.github.alersrt.pod4j.openapi.api.PodsApi;
+import okhttp3.OkHttpClient;
+import okhttp3.unixdomainsockets.UnixDomainSocketFactory;
+
+import java.io.File;
+import java.net.UnixDomainSocketAddress;
+import java.net.http.HttpClient;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) throws ApiException {
-        var apiClient = Configuration.getDefaultApiClient()
-                .setBasePath("http+unix://run/user/1000/podman/podman.sock");
+        var socketFile = new File("http://run/user/1000/podman/podman.sock");
+        var httpClient = new OkHttpClient.Builder()
+                .socketFactory(new UnixDomainSocketFactory(socketFile))
+                .build();
+
+        var apiClient = new ApiClient(httpClient).setBasePath("http://d/v5.0.0/libpod/");
         var pods = new PodsApi(apiClient);
         pods.podListLibpod("").forEach(System.out::println);
 
