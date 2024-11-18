a100cf5536e508a375a3c1900b7ff6f70d1ff30b
Aleksandr <strtmg@gmail.com>
2024-11-18 19:28:50 +0700

    0.1.0

--- src/main/java/io/github/alersrt/pod4j/Main.java
+++ src/main/java/io/github/alersrt/pod4j/Main.java
@@ -9,7 +9,7 @@ import io.github.alersrt.pod4j.openapi.api.PodsApi;
 public class Main {
     public static void main(String[] args) throws ApiException {
         var apiClient = Configuration.getDefaultApiClient()
-                .setBasePath("unix://run/user/1000/podman/podman.sock");
+                .setBasePath("http+unix://run/user/1000/podman/podman.sock");
         var pods = new PodsApi(apiClient);
         pods.podListLibpod("").forEach(System.out::println);
 
