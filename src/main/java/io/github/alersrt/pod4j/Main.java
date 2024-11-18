52a99f141588e5b1a749d31999ffa4665cd1cd1e
Aleksandr <strtmg@gmail.com>
2024-11-18 23:01:45 +0700

    0.1.0

--- src/main/java/io/github/alersrt/pod4j/Main.java
+++ src/main/java/io/github/alersrt/pod4j/Main.java
@@ -4,8 +4,10 @@ package io.github.alersrt.pod4j;
 import io.github.alersrt.pod4j.openapi.ApiClient;
 import io.github.alersrt.pod4j.openapi.ApiException;
 import io.github.alersrt.pod4j.openapi.Configuration;
+import io.github.alersrt.pod4j.openapi.api.ImagesApi;
 import io.github.alersrt.pod4j.openapi.api.PodsApi;
 import okhttp3.OkHttpClient;
+import okhttp3.Protocol;
 import okhttp3.unixdomainsockets.UnixDomainSocketFactory;
 
 import java.io.File;
@@ -13,17 +15,19 @@ import java.net.UnixDomainSocketAddress;
 import java.net.http.HttpClient;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws ApiException {
-        var socketFile = new File("http://run/user/1000/podman/podman.sock");
+        var socketFile = new File("/run/user/1000/podman/podman.sock");
         var httpClient = new OkHttpClient.Builder()
                 .socketFactory(new UnixDomainSocketFactory(socketFile))
+                .protocols(List.of(Protocol.H2_PRIOR_KNOWLEDGE))
                 .build();
 
         var apiClient = new ApiClient(httpClient).setBasePath("http://d/v5.0.0/libpod/");
-        var pods = new PodsApi(apiClient);
-        pods.podListLibpod("").forEach(System.out::println);
+        var imagesApi = new ImagesApi(apiClient);
+        imagesApi.imageListLibpod(true, null).forEach(System.out::println);
 
         System.out.println("Hello world!");
     }
