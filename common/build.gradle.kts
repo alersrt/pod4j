432df80c10694d7faae3fb69639ca0e37d59f9ef
Aleksandr <strtmg@gmail.com>
2024-11-20 21:37:53 +0700

    0.1.0

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -11,9 +11,9 @@ repositories {
 }
 
 dependencies {
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("io.github.alersrt:pod4j.openapi-java:5.0.0")
     implementation("com.squareup.okhttp3.sample:unixdomainsockets:3.14.9")
-    implementation(project(":openapi-java"))
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
     testImplementation(platform("org.junit:junit-bom:5.10.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
--- settings.gradle.kts
+++
@@ -1,3 +0,0 @@
-rootProject.name = "pod4j"
-
-include(":openapi-java")
