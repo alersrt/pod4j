121f9b30cfc2a90d99fa7b1a66ed8fc09dfa025e
Aleksandr <strtmg@gmail.com>
2024-12-03 17:54:30 +0700

    0.1.0

--- common/src/main/java/io/github/alersrt/pod4j/App.java
+++ common/src/main/java/io/github/alersrt/pod4j/App.java
@@ -9,9 +9,12 @@ import java.nio.file.Paths;
 public class App {
 
     public static void main(String[] args) throws ApiException, IOException, InterruptedException {
-        var podman = new KubePlayer("/var/run/user/1000/podman/podman.sock");
-        podman.start(Paths.get("common/src/main/resources/nginx.yaml").toAbsolutePath().toString());
-        Thread.sleep(10 * 1000);
-        podman.stop(Paths.get("common/src/main/resources/nginx.yaml").toAbsolutePath().toString());
+        var podman = new KubePlayer(
+                "/var/run/user/1000/podman/podman.sock",
+                Paths.get("common/src/main/resources/nginx.yaml").toAbsolutePath().toString()
+        );
+        podman.start();
+        System.out.println(podman.getMappedPort("nginx", "80"));
+        podman.stop();
     }
 }
--- common/src/main/java/io/github/alersrt/pod4j/KubePlayer.java
+++ common/src/main/java/io/github/alersrt/pod4j/KubePlayer.java
@@ -3,7 +3,12 @@ package io.github.alersrt.pod4j;
 import io.github.alersrt.pod4j.exceptions.PodmanException;
 import io.github.alersrt.pod4j.openapi.ApiClient;
 import io.github.alersrt.pod4j.openapi.ApiException;
+import io.github.alersrt.pod4j.openapi.api.ContainersApi;
 import io.github.alersrt.pod4j.openapi.api.PodsApi;
+import io.github.alersrt.pod4j.openapi.model.InspectContainerData;
+import io.github.alersrt.pod4j.openapi.model.InspectContainerHostConfig;
+import io.github.alersrt.pod4j.openapi.model.PlayKubePod;
+import io.github.alersrt.pod4j.openapi.model.PlayKubeReport;
 import okhttp3.OkHttpClient;
 import okhttp3.unixdomainsockets.UnixDomainSocketFactory;
 
@@ -12,17 +17,23 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.UUID;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Stream;
 
 public class KubePlayer {
 
     private final ApiClient api;
+    private final String yamlPath;
 
-    public KubePlayer() throws PodmanException {
-        this(System.getenv(Constants.ENV_PODMAN_SOCKET));
+    private PlayKubeReport report = null;
+
+    public KubePlayer(String yamlPath) throws PodmanException {
+        this(System.getenv(Constants.ENV_PODMAN_SOCKET), yamlPath);
     }
 
-    public KubePlayer(String socketPath) throws PodmanException {
+    public KubePlayer(String socketPath, String yamlPath) throws PodmanException {
         if (socketPath == null || socketPath.isEmpty()) {
             throw new PodmanException("Environment variable " + Constants.ENV_PODMAN_SOCKET + " is not set");
         }
@@ -34,12 +45,14 @@ public class KubePlayer {
         this.api = new ApiClient()
                 .setHttpClient(httpClient)
                 .setBasePath("http://localhost/v5.0.0");
+
+        this.yamlPath = yamlPath;
     }
 
-    public void start(String yamlPath) throws ApiException, IOException {
-        final var pods = new PodsApi(api);
-        String yaml = readFile(yamlPath);
-        pods.playKubeLibpod(
+    public void start() throws ApiException, IOException {
+        final var pods = new PodsApi(this.api);
+        String yaml = readFile(this.yamlPath);
+        this.report = pods.playKubeLibpod(
                 null,
                 null,
                 null,
@@ -48,7 +61,7 @@ public class KubePlayer {
                 false,
                 false,
                 null,
-                false,
+                true,
                 true,
                 true,
                 true,
@@ -60,12 +73,51 @@ public class KubePlayer {
                 false,
                 yaml
         );
+
+        if (this.report == null || this.report.getPods() == null || this.report.getPods().isEmpty()) {
+            throw new PodmanException("There is no related pods");
+        }
+        if (this.report.getPods().size() > 1) {
+            throw new PodmanException("There should be only one pod");
+        }
     }
 
-    public void stop(String yamlPath) throws ApiException, IOException {
-        final var pods = new PodsApi(api);
-        String yaml = readFile(yamlPath);
+    public void stop() throws ApiException, IOException {
+        final var pods = new PodsApi(this.api);
+        String yaml = readFile(this.yamlPath);
         pods.playKubeDownLibpod(null, true, yaml);
+    }
+
+    public Integer getMappedPort(String containerName, String containerPort) throws ApiException {
+        var containerApi = new ContainersApi(api);
+
+        InspectContainerData data = null;
+        for (PlayKubePod playKubePod : this.report.getPods()) {
+            if (playKubePod.getContainers() != null) {
+                for (String container : playKubePod.getContainers()) {
+                    data = containerApi.containerInspectLibpod(container, true);
+                    if (data != null && data.getId() != null && data.getId().equals(container)) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (data == null) {
+            throw new PodmanException("There is no related containers");
+        }
+
+        return Stream.ofNullable(data.getHostConfig())
+                .map(InspectContainerHostConfig::getPortBindings)
+                .filter(Objects::nonNull)
+                .map(Map::entrySet)
+                .flatMap(Set::stream)
+                .filter(bind -> bind.getKey().matches("^%s/(tcp|upd)$".formatted(containerPort)))
+                .map(bind -> bind.getValue().get(0).getHostPort())
+                .filter(Objects::nonNull)
+                .map(Integer::parseInt)
+                .findAny()
+                .orElse(null);
     }
 
     private String readFile(String filename) throws IOException {
